14ac091165feca26e855581c4eb8d1b6f76a1fcf
Muriel Berthou Crestey <murielberthoucrestey@gmail.com>
2016-11-28 14:40:22 +0100

    changements-front

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ puts 'generate new resources'
 admin = User.create(email:'admin@visum.com', password:'123soleil')
 doe = User.create(email:'lambda@visum.com', password:'123soleil')
 
+découverte = Category
 nature = Category.create(name: 'nature')
 insolite = Category.create(name: 'insolite')
 monument = Category.create(name: 'monument')
