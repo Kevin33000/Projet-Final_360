9685c231278f2d82a5c1a984436472c9ba22975a
Kevin Marquenet <kevin.marquenet@gmail.com>
2016-11-28 14:34:40 +0100

    instalation-plugin-Spherique-+-front

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -2,7 +2,10 @@ class Poi < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many   :reviews
-  has_attachments :photos, maximum: 2
+  has_attachment :photo
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+
+  validates :category, presence: true
+  validates :user, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ monument = Category.create(name: 'monument')
     category: insolite,
     user: doe,
     address: "Rue de bdx, 33000 BORDEAUX",
-    caption: "http://www.bordeaux-tourisme.com/var/ezwebin_site/storage/images/media/images/bordeaux-gp/rendez-vous-a-bordeaux/684165-7-fre-FR/Rendez-vous-a-Bordeaux_format_1140x522.jpg")
+    caption: "la magie, splendide !")
 end
 
 pois = [
@@ -29,14 +29,14 @@ pois = [
     category: insolite,
     user: admin,
     address: "107 Cours Balguerie Stuttenberg",
-    caption: "http://static.latribune.fr/full_width/521995/le-wagon-formation-ccib.jpg"
+    caption: "c'est un lieu fantastique"
   },
   {
     title: "Auchan",
     category: monument,
     user: admin,
     address: "57 Rue du Château d'Eau",
-    caption: "http://images.google.fr/imgres?imgurl=http%3A%2F%2Fwww.justacote.com%2Fphotos_entreprises%2Fauchan-bordeaux-1322256790.jpg&imgrefurl=http%3A%2F%2Fwww.justacote.com%2Fbordeaux-33000%2Fsuperette-et-supermarche%2Fauchan-908143.htm&h=600&w=800&tbnid=oOKPCnP0w9-ewM%3A&vet=1&docid=OTOFKUzrjix_3M&ei=C4I0WI77MsuRgAbVmqmQCA&tbm=isch&iact=rc&uact=3&dur=2448&page=0&start=0&ndsp=15&ved=0ahUKEwjOrOSq87zQAhXLCMAKHVVNCoIQMwgeKAEwAQ&bih=673&biw=1301"
+    caption: "la vie est moins cher"
   }
 ]
 
