ad91c6c61c9bb51bf9d32ea6ad18a5b2e9ad4c43
Kevin Marquenet <kevin.marquenet@gmail.com>
2016-12-01 18:29:23 +0100

    remove user from project (for now)

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,12 +1,12 @@
 class Poi < ApplicationRecord
   belongs_to :category
-  belongs_to :user
+  # belongs_to :user
   has_many   :reviews, dependent: :destroy
   has_attachment :photo
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
   validates :category, presence: true
-  validates :user, presence: true
+  # validates :user, presence: true
   # validates :photo, presence: true
 end
